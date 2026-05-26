026deceb66a83f1c034a027da996bb7c82fd8027
Sagar06 <sagund2000@gmail.com>
2026-05-27 01:16:16 +0530

    Add About and Contact pages with navigation links components

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      <Link href={"/contact"}>Go to contact</Link>
+      <br />
+      <Link href={"/products/sagund"}>Go to product</Link>
+      <Image 
+      src={'/globe.svg'}
+      width={200}
+      height={250}
+      alt="picture of globe"
+       />
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
